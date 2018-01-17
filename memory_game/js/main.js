e73e4edc09e3f5fc5b3e91eb5bb7d1c69742b364
Hannah Aurand <hannahaurand@Hannahs-MacBook-Pro.local>
2018-01-16 23:53:09 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,24 +32,43 @@ console.log("Sorry, try again.");
 }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function (/*cardId*/) {
 
+var cardId =  this.getAttribute("data-id");
 console.log("User flipped " + cards[cardId].rank + ".");
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 
 cardsInPlay.push(cards[cardId].rank);
 
+this.setAttribute("src", cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
  checkForMatch();
 };
-
-
-
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i< cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i); 
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+//function reset () {
+	//console.log("reset")
+	//createBoard();
+	//var button = document.getElementByTag('button');
+	//button.addEventListener('click', createBoard);
+//};
+
+//flipCard(0);
+//flipCard(2);
+
+createBoard();
 
 
 
